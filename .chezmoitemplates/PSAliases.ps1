666f85a7e67eae0005dad240b3f14cb95a3cc205
tleepa <tleepa@gmail.com>
2022-10-12 13:23:42 +0200

    missing backslash

--- .chezmoitemplates/PSAliases.ps1
+++ .chezmoitemplates/PSAliases.ps1
@@ -39,6 +39,6 @@ if (scoop shim list privoxy) {
 }
 
 if (scoop shim list wsl-ssh-agent) {
-    "$(scoop prefix wsl-ssh-agent) wsl-ssh-agent-gui -socket $env:USERPROFILE.\.keepassxc.sock" | Invoke-Expression
+    "$(scoop prefix wsl-ssh-agent)\wsl-ssh-agent-gui -socket $env:USERPROFILE.\.keepassxc.sock" | Invoke-Expression
 }
 {{- end -}}
