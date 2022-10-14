46d8ab46ad48b1a5c199880a8397c8fa38fcf32d
tleepa <tleepa@gmail.com>
2022-10-14 16:38:07 +0200

    fx customizations

---
+++ dot_mozilla/customizations/user-overrides/leepa.js
@@ -0,0 +1,30 @@
+// leepa overrides
+
+user_pref("accessibility.typeaheadfind", true);
+user_pref("browser.ctrlTab.sortByRecentlyUsed", false);
+user_pref("browser.display.use_system_colors", true);
+user_pref("browser.download.alwaysOpenPanel", true);
+user_pref("browser.download.useDownloadDir", true);
+user_pref("browser.newtabpage.enabled", true);
+user_pref("browser.search.suggest.enabled", true);
+user_pref("browser.startup.homepage", "about:home");
+user_pref("browser.startup.page", 3);
+user_pref("browser.tabs.closeWindowWithLastTab", false);
+user_pref("browser.urlbar.suggest.searches", true);
+user_pref("extensions.pocket.enabled", false);
+user_pref("general.autoScroll", true);
+user_pref("gfx.direct3d11.reuse-decoder-device", false);
+user_pref("gfx.webrender.all", true);
+user_pref("intl.accept_languages", "pl, en-US, en");
+user_pref("layout.css.visited_links_enabled", true);
+user_pref("media.eme.enabled", true);
+user_pref("media.ffmpeg.vaapi.enabled", true);
+user_pref("media.wmf.zero-copy-nv12-texture", false);
+user_pref("network.cookie.lifetimePolicy", 0);
+user_pref("privacy.cpd.history", false);
+user_pref("privacy.cpd.sessions", false);
+user_pref("privacy.resistFingerprinting", false);
+user_pref("privacy.resistFingerprinting.letterboxing", false);
+user_pref("privacy.sanitize.sanitizeOnShutdown", false);
+user_pref("signon.rememberSignons", false);
+user_pref("webgl.disabled", false);
