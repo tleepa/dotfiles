0be6e8d157dd080232404711822616f6c4ecf5ea
tleepa <tleepa@gmail.com>
2022-10-30 14:06:52 +0100

    disable firefox-view

--- dot_mozilla/customizations/user-overrides/leepa.js
+++ dot_mozilla/customizations/user-overrides/leepa.js
@@ -10,6 +10,8 @@ user_pref("browser.search.suggest.enabled", true);
 user_pref("browser.startup.homepage", "about:home");
 user_pref("browser.startup.page", 3);
 user_pref("browser.tabs.closeWindowWithLastTab", false);
+user_pref("browser.tabs.firefox-view", false);
+user_pref("browser.tabs.tabmanager.enabled", false);
 user_pref("browser.urlbar.suggest.searches", true);
 user_pref("extensions.pocket.enabled", false);
 user_pref("general.autoScroll", true);
