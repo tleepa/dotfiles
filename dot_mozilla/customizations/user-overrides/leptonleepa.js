554e3fc9c897095a4b17cd1103e0cf5cf5d38680
tleepa <tleepa@gmail.com>
2023-09-04 14:16:06 +0200

    theme overrides

---
+++ dot_mozilla/customizations/user-overrides/leptonleepa.js
@@ -0,0 +1,6 @@
+// lepton overrides
+
+user_pref("userChrome.icon.global_menubar", false);
+user_pref("userChrome.padding.global_menubar", false);
+user_pref("userChrome.theme.non_native_menu", false);
+user_pref("userChrome.compatibility.os.linux_non_native_titlebar_button", false);
