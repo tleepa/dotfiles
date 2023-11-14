b3514bc71c9a74d21a1af5a62d0603824fad00fe
tleepa <tleepa@gmail.com>
2023-11-14 12:01:20 +0100

    added function inserting matching quotes

--- .chezmoitemplates/Microsoft.PowerShell_profile.ps1
+++ .chezmoitemplates/Microsoft.PowerShell_profile.ps1
@@ -40,6 +40,26 @@ Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
 Set-PSReadLineOption -ShowToolTips
 Set-PSReadLineOption -PredictionSource history
 
+# Smart quotes
+Set-PSReadLineKeyHandler -Chord '"',"'" -BriefDescription SmartInsertQuote -LongDescription "Insert paired quotes if not already on a quote" -ScriptBlock {
+    param($key, $arg)
+
+    $line = $null
+    $cursor = $null
+    [Microsoft.PowerShell.PSConsoleReadLine]::GetBufferState([ref]$line, [ref]$cursor)
+
+    if ($line.Length -gt $cursor -and $line[$cursor] -eq $key.KeyChar) {
+        # Just move the cursor
+        [Microsoft.PowerShell.PSConsoleReadLine]::SetCursorPosition($cursor + 1)
+    }
+    else {
+        # Insert matching quotes, move cursor to be in between the quotes
+        [Microsoft.PowerShell.PSConsoleReadLine]::Insert("$($key.KeyChar)" * 2)
+        [Microsoft.PowerShell.PSConsoleReadLine]::GetBufferState([ref]$line, [ref]$cursor)
+        [Microsoft.PowerShell.PSConsoleReadLine]::SetCursorPosition($cursor - 1)
+    }
+}
+
 Set-PSReadlineOption -Color @{
     Command          = [ConsoleColor]::Green
     Parameter        = [ConsoleColor]::Gray
