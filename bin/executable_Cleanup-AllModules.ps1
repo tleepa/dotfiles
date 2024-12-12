dbba0b539191b5a0db46fa45fb230b94dc7d620c
tleepa <7734919+tleepa@users.noreply.github.com>
2024-12-12 17:44:02 +0100

    cleanup ps modules

--- bin/executable_Cleanup-AllModules.ps1
+++ bin/executable_Cleanup-AllModules.ps1
@@ -3,21 +3,13 @@ param (
     [switch] $Noop
 )
 
-if (Get-InstalledModule -Name 'Az.Tools.Installer') {
-    $Modules = Get-InstalledModule | Where-Object { $_.Name -notmatch '^Az' }
-} else {
-    $Modules = Get-InstalledModule
-}
+$Modules = Get-InstalledModule
 
 Write-Host "Processing modules..."
-$Modules | ForEach-Object {
+$Modules | ForEach-Object -ThrottleLimit 10 -Parallel {
     $ModName = $_.Name
-    Write-Progress -Activity $ModName -PercentComplete (($Modules.IndexOf($_) + 1) / $Modules.Count * 100)
-    $CurModule = Get-InstalledModule -Name $ModName -AllVersions | Sort-Object -Property Version -Descending
-
-    if ($CurModule.length -gt 1) {
-        $CurModule[1..$CurModule.Length] | ForEach-Object {
-            Uninstall-Module -Name $ModName -RequiredVersion $_.Version -WhatIf:$Noop
-        }
+    Write-Progress -Activity $ModName -PercentComplete (($($using:Modules).IndexOf($_) + 1) / $($using:Modules).Count * 100)
+    $CurModule = Get-InstalledModule -Name $ModName -AllVersions | Select-Object -SkipLast 1 | ForEach-Object {
+        Uninstall-Module -Name $ModName -RequiredVersion $_.Version -Force -WhatIf:$using:Noop
     }
 }
