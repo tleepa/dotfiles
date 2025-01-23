be92c3322a8998b2c86467e257d41cfaf492effc
tleepa <7734919+tleepa@users.noreply.github.com>
2025-01-23 13:08:02 +0100

    fixes for ssh agent on WSL

--- .chezmoitemplates/PSAliases.ps1
+++ .chezmoitemplates/PSAliases.ps1
@@ -56,10 +56,6 @@ if ($scoop_shims | Where-Object { $_.Name -eq "privoxy" }) {
     }
 }
 
-# if (($scoop_shims | Where-Object { $_.Name -eq "wsl-ssh-agent-gui" }) -and (-not (Get-Process -Name "wsl-ssh-agent-gui" -ErrorAction SilentlyContinue))) {
-#     "$(get_scoop_app_path "wsl-ssh-agent-gui")\wsl-ssh-agent-gui -socket $env:USERPROFILE.\.keepassxc.sock" | Invoke-Expression
-# }
-
 if (Get-Command -Name rg -ErrorAction SilentlyContinue) {
     Set-Alias -Name grep -Value rg
 }
