185da0505c0789dd7b9571d5fbe8798a821c243e
tleepa <7734919+tleepa@users.noreply.github.com>
2025-12-16 16:11:57 +0100

    fix PS modules cleanup

--- bin/executable_Cleanup-AllModules.ps1
+++ bin/executable_Cleanup-AllModules.ps1
@@ -9,7 +9,7 @@ Write-Host "Processing modules..."
 $Modules | ForEach-Object -ThrottleLimit 10 -Parallel {
     $ModName = $_.Name
     Write-Progress -Activity $ModName -PercentComplete (($($using:Modules).IndexOf($_) + 1) / $($using:Modules).Count * 100)
-    $CurModule = Get-InstalledModule -Name $ModName -AllVersions | Sort-Object -Property Version -Descending | Select-Object -SkipLast 1 | ForEach-Object {
+    $CurModule = Get-InstalledModule -Name $ModName -AllVersions | Sort-Object -Property Version | Select-Object -SkipLast 1 | ForEach-Object {
         Uninstall-Module -Name $ModName -RequiredVersion $_.Version -Force -WhatIf:$using:Noop
     }
 }
