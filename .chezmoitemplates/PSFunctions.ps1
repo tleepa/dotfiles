e36973cfbf88edb3378113a84304a63e374f808f
tleepa <7734919+tleepa@users.noreply.github.com>
2025-12-23 12:19:53 +0100

    move Update and Cleanup AllModules to functions

--- .chezmoitemplates/PSFunctions.ps1
+++ .chezmoitemplates/PSFunctions.ps1
@@ -194,3 +194,44 @@ function Get-SshSessions {
     Get-Process | Where-Object { $_.Name -eq 'ssh' -and $_.CommandLine -match 'ssh' } `
     | Select-Object -Property Id, Name, CommandLine
 }
+
+function Update-AllModules {
+    param (
+        [switch] $Noop
+    )
+
+    $Modules = Get-InstalledModule | Where-Object { $_.Name -notmatch '^Az\.' -and $_.Name -notmatch 'VMWare\.' -or $_.Name -match 'VMWare\.PowerCLI' }
+    # $Modules.Count
+
+    if (Get-InstalledModule -Name 'Az.Tools.Installer') {
+        Write-Host "Processing AZ modules..."
+        Update-AzModule -Repository PSGallery -WhatIf:$Noop
+    } else {
+        $Modules += Get-InstalledModule | Where-Object { $_.Name -eq 'Az' }
+    }
+
+    Write-Host "Processing modules..."
+    $Modules | ForEach-Object {
+        $ModName = $_.Name
+        Write-Progress -Activity $ModName -PercentComplete (($Modules.IndexOf($_) + 1) / $Modules.Count * 100)
+        Update-Module -Name $ModName -Scope CurrentUser -WhatIf:$Noop
+    }
+}
+
+function Cleanup-AllModules {
+    param (
+        [switch] $Noop
+    )
+
+    $Modules = Get-InstalledModule
+
+    Write-Host "Processing modules..."
+    $Modules | ForEach-Object -ThrottleLimit 10 -Parallel {
+        $ModName = $_.Name
+        Write-Progress -Activity $ModName -PercentComplete (($($using:Modules).IndexOf($_) + 1) / $($using:Modules).Count * 100)
+        $CurModule = Get-InstalledModule -Name $ModName -AllVersions | Sort-Object -Property Version | Select-Object -SkipLast 1 | ForEach-Object {
+            Uninstall-Module -Name $ModName -RequiredVersion $_.Version -Force -WhatIf:$using:Noop
+        }
+    }
+}
+
