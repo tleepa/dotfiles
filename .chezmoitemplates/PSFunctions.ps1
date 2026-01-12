45ff582b9a150649fc2653a600a9ca7505647473
tleepa <7734919+tleepa@users.noreply.github.com>
2026-01-12 11:50:15 +0100

    fix versions sort

--- .chezmoitemplates/PSFunctions.ps1
+++ .chezmoitemplates/PSFunctions.ps1
@@ -229,7 +229,7 @@ function Cleanup-AllModules {
     $Modules | ForEach-Object -ThrottleLimit 10 -Parallel {
         $ModName = $_.Name
         Write-Progress -Activity $ModName -PercentComplete (($($using:Modules).IndexOf($_) + 1) / $($using:Modules).Count * 100)
-        $CurModule = Get-InstalledModule -Name $ModName -AllVersions | Sort-Object -Property Version | Select-Object -SkipLast 1 | ForEach-Object {
+        $CurModule = Get-InstalledModule -Name $ModName -AllVersions | Sort-Object -Property { [Version]$_.Version } | Select-Object -SkipLast 1 | ForEach-Object {
             Uninstall-Module -Name $ModName -RequiredVersion $_.Version -Force -WhatIf:$using:Noop
         }
     }
